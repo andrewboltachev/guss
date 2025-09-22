c40fbd813351c7eccd830f4e4b0cf79490e67e98
Andrei Boltachev <andrewboltachev@gmail.com>
2025-09-22 21:02:16 +0300

    validations

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,6 +4,8 @@ import {
   Controller,
   Post,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -13,7 +15,6 @@ import { LoginOrRegisterDto } from '../login_or_register.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Body() loginOrRegisterDto: LoginOrRegisterDto) {
     const userOrNull = await this.authService.validateUser(loginOrRegisterDto);
--- backend/src/login_or_register.dto.ts
+++ backend/src/login_or_register.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class LoginOrRegisterDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
 }
